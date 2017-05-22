e6120ff822ee8efa88853ed55c54ede43ac38c19
Thys Meintjes <sthysel@gmail.com>
2017-05-22 12:50:13 +0800

    Bump version: 0.2.11 → 0.2.12

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = 'knobs'
 year = '2017'
 author = 'sthysel'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.2.11'
+version = release = '0.2.12'
 
 pygments_style = 'trac'
 templates_path = ['.']
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(*names, **kwargs):
 setup(
     name='knobs',
     license='GPLv2',
-    version='0.2.11',
+    version='0.2.12',
     description='Environment variable manager',
     long_description=read('README.rst'),
     install_requires=[
