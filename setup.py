41067ca9dcb31e937f349f87e2e49522755cc7c4
Thys Meintjes <sthysel@gmail.com>
2017-05-22 11:37:15 +0800

    Bump version: 0.2.10 → 0.2.11

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(*names, **kwargs):
 setup(
     name='knobs',
     license='GPLv2',
-    version='0.2.10',
+    version='0.2.11',
     description='Environment variable manager',
     long_description=read('README.rst'),
     install_requires=[
