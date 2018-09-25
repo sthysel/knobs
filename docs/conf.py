8aa3c53861cd71bd6b35ba4f285db3c2b6c4fd08
Thys Meintjes <sthysel@gmail.com>
2018-09-25 10:37:40 +0800

    Bump version: 0.2.16 → 1.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ project = 'knobs'
 year = '2017'
 author = 'sthysel'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.2.16'
+version = release = '1.0.0'
 
 pygments_style = 'trac'
 templates_path = ['.']
